661be732e126092bcf088436ee73d2f823783375
MazenHossam <60198948+MazenHossam@users.noreply.github.com>
2022-04-13 22:42:05 +0200

    Create the_minion_game_SOL.py

---
+++ The minion game/the_minion_game_SOL.py
@@ -0,0 +1,19 @@
+def minion_game(string):
+    string=list(string)
+    
+    STUART=0
+    KEVIN=0
+    
+    #con=[e for e in string if e not in ('A','O','U','I','E')]   # B N N 
+    #vow=[e for e in string if e in ('A','O','U','I','E')]       # A A A
+    
+    for i in range(len(string)):
+        if string[i] not in ('A','O','U','I','E'):
+            STUART+= len(string)-i
+        else:
+            KEVIN+= len(string)-i
+    
+    if KEVIN>STUART: print("Kevin {}".format(KEVIN))
+    elif KEVIN<STUART: print("Stuart {}".format(STUART))
+    else: print("Draw")
+
