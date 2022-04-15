5af2c714a24bbdb62cb3c6877c0f8c70e44f1ea2
MazenHossam <60198948+MazenHossam@users.noreply.github.com>
2022-04-15 22:45:04 +0200

    Create no_idea_SOL.py

---
+++ No idea/no_idea_SOL.py
@@ -0,0 +1,13 @@
+n,m=input().split()
+arr=list(input().split())
+A=set(input().split())
+B=set(input().split())
+happiness=0
+
+for i in arr:
+    if i in A:
+        happiness+=1
+    elif i in B:
+        happiness-=1
+        
+print(happiness)
